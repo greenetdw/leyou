17810da60bd045bfaaae6fa51d44a83ad34cf691
470277363@qq.com <470277363@qq.com>
2018-09-12 11:22:35 +0800

    init

---
+++ ly-api-gateway/src/main/java/com/leyou/LyApiGateway.java
@@ -0,0 +1,18 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by 01375677 on 2018/9/10.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class LyApiGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(LyApiGateway.class, args);
+    }
+}
